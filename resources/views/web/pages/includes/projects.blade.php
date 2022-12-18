408f5fce7c254896654ecf80c013d6fb021bdc6c
Sasha1Revnuk <revo0708@gmail.com>
2022-12-18 13:54:36 +0200

    add projects

--- resources/views/web/pages/includes/projects.blade.php
+++ resources/views/web/pages/includes/projects.blade.php
@@ -1,7 +1,15 @@
 <div class="row">
     @foreach($projects as $project)
         <div class=" col-md-4 list">
-            <div class="pic"><a target="_blank" href="{{route('events.single', ['event' => $project->slug])}}"><img
+            <div class="pic"><a style=" position: relative;
+    padding-top: 56%; display: flex" target="_blank" href="{{route('events.single', ['event' => $project->slug])}}"><img
+                            style="position: absolute;
+    top: 0;
+    left: 0;
+    object-fit: cover;
+    object-position: center;
+    width: 100%;
+    height: 100%;"
                             src="{{$project->image_url}}" class="img-responsive"></a></div>
             <div class="event-ct">
                 <h2>
