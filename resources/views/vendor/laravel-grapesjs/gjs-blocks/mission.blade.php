be5e6956131291af969835fa800ccef37dba0c0c
Sasha1Revnuk <revo0708@gmail.com>
2022-12-18 12:51:48 +0200

    add blocks

---
+++ resources/views/vendor/laravel-grapesjs/gjs-blocks/mission.blade.php
@@ -0,0 +1,29 @@
+<div class="row">
+    <div class="col-md-3">
+        <div class="blk"><i class="fa fa-bell  icon-size"></i>
+            <h2 class="item-title">Our Mission</h2>
+            <p> Donec vel sollicitudin nisl, quis dignissim elit. Nunc laoreet nisi non odio tempus, at
+                .</p>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="blk"><i class="fa fa-file-text icon-size"></i>
+            <h2 class="item-title">Education</h2>
+            <p>Sed mattis dui ut ultrices sodales. Integer lacinia lectus quis nnon accumsan dui
+                molestie</p>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="blk"><i class="fa fa-life-ring icon-size"></i>
+            <h2 class="item-title">Help &amp; support</h2>
+            <p>The arrangement of type involves the selection of typefaces point size line length
+                leading. </p>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="blk"><i class="fa fa-money icon-size"></i>
+            <h2 class="item-title">Make Donations</h2>
+            <p>Typography is the art and technique of arranging type in order to make language visible.</p>
+        </div>
+    </div>
+</div>
---
+++ resources/views/vendor/laravel-grapesjs/gjs-blocks/testimonials.blade.php
@@ -0,0 +1,10 @@
+<div class="testimonials">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-2 col-md-8">
+                <h1>Success Story</h1>
+                testimonials_placeholder
+            </div>
+        </div>
+    </div>
+</div>
