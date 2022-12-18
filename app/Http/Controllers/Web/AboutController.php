a153934c74cc10ebcd6cad10c92e01b1e614837a
Sasha1Revnuk <revo0708@gmail.com>
2022-12-18 12:12:01 +0200

    add fixes

--- app/Http/Controllers/Web/AboutController.php
+++ app/Http/Controllers/Web/AboutController.php
@@ -14,6 +14,7 @@ class AboutController extends Controller
 
     public function index()
     {
+        abort(404);
         $page = Page::where('slug', PageEnumerator::ABOUTPAGE_SLUG)->withTranslations([app()->getLocale()])->first();
         $pageTranslate = $page->translations->first();
         $data = [
--- resources/views/layouts/web/partials/footer.blade.php
+++ resources/views/layouts/web/partials/footer.blade.php
@@ -1,10 +1,21 @@
 <div class="sub-footer">
     <div class="container">
         <div class="row">
-            <div class="col-md-10">
-                <p>© <script>document.write(new Date().getFullYear())</script> All rights reserved. </p>
+            <div class="col-md-12">
+                <ul class="list-style">
+
+                    <li><i class="fa fa-phone-square"></i> +34.633.584.738</li>
+                    <li><i class="fa fa-envelope"></i> culturacontrabalas@gmail.com</li>
+                </ul>
             </div>
-            <div class="col-md-2"><a target="__blank" href="https://www.instagram.com/culturacontrabalas/"><i class="fa fa-instagram"></i></a></div>
+            <div class="col-md-10">
+                <p>©
+                    <script>document.write(new Date().getFullYear())</script>
+                    All rights reserved.
+                </p>
+            </div>
+            <div class="col-md-2"><a target="__blank" href="https://www.instagram.com/culturacontrabalas/"><i
+                            class="fa fa-instagram"></i></a></div>
         </div>
     </div>
 </div>
