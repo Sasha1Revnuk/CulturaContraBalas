b644a9ddc3df626e410ce467a14fa70338b6950a
Sasha1Revnuk <revo0708@gmail.com>
2022-12-21 10:48:43 +0200

    add index folow and go

--- resources/views/layouts/web/partials/headerSeo.blade.php
+++ resources/views/layouts/web/partials/headerSeo.blade.php
@@ -4,5 +4,5 @@
 <meta name="description" content="{{isset($meta['description']) ? $meta['description']  : env('APP_NAME')}}">
 <meta name="keywords" content="{{isset($meta['keywords']) ? $meta['keywords']  : env('APP_NAME')}}">
 {{--<meta name="robots" content="{{isset($meta['robots']) ? $meta['robots'] : env('APP_NAME')}}">--}}
-<meta name="robots" content="noindex, nofollow">
+<meta name="robots" content="index, follow">
 <meta name="csrf-token" content="{{ csrf_token() }}">
